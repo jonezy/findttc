0712a6d65159d6353ea937ae6b8666619aebaa23
Chris Jones <chris@jonezy.org>
2012-11-04 13:32:05 -0500

    some cleanup before merging
    Signed-off-by: Chris Jones <chris@jonezy.org>

---
+++ src/j/lib/backbone.close.js
@@ -0,0 +1,26 @@
+// adds a close function to Backbone.View, if you need to do anything
+// specific when closing your view, implement an onClose function.
+Backbone.View.prototype.close = function () {
+  // close all the child views that have been stored in this.childViews
+  _.each(this.childViews, function (childView) {
+    if (childView.remove) {
+      childView.remove();
+    }
+
+    if (childView.close) {
+      childView.close();
+    }
+
+    delete childView;
+  });
+
+  // handle cleaning up this view
+  this.off();
+  this.remove();
+
+  if (this.onClose) {
+    this.onClose();
+  }
+
+  return this;
+}
